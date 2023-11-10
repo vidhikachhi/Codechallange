ebf40529da5e27bd895786b9289f7161e59ad769
Vidhi kachhi <118750257+vidhikachhi@users.noreply.github.com>
2023-11-10 19:28:23 +0530

    Update

---
+++ string2.cpp
@@ -0,0 +1,33 @@
+include <iostream>
+using namespace std;
+int main() {
+      int t;
+      cin >> t;
+      while(t--)
+      {
+          int n;
+          cin >> n;
+          string s;
+          cin >> s;
+          int alice_marks=0;
+          int bob_marks=0;
+          if(s[0]=='A') 
+              alice_marks++;
+              for(int i=0;i<n;i++)
+              {
+                  if(s[i]=='A'&&s[i+1]=='A')
+                  {
+                      alice_marks++;
+                  }
+                  
+                  else if(s[i]=='B'&&s[i+1]=='B')
+                  {
+                      bob_marks++;
+                  }
+                  else continue;
+              }
+  cout << alice_marks<< " " << bob_marks << endl;
+
+      }
+	return 0;
+}
