7db007e31ed4141c348e6e23deb58be1e66b3ac4
Vidhi kachhi <118750257+vidhikachhi@users.noreply.github.com>
2023-11-26 11:41:17 +0530

    Update smallest.cpp

--- smallest.cpp
+++ smallest.cpp
@@ -5,22 +5,16 @@ int main() {
     int t; 
     cin>>t;
 	while(t>=0){
-	    int n,k,m=0;
-	    cin>>n;
-	    cin>>k;
-	        if(k>n)
-	        {
-	         cout<<n<<endl;
-	         break;
-	        }
+	    int n,k;
+	    cin>>n>>k;
+	        if(k>n){
+	         cout<<n<<endl;break;}
 	    else if(n>k)
-	    {
-	    m=n%k;
-	    cout<<m<<endl;
-	    }
+	    cout<<n%k<<endl;
 	    else
 	    cout<<0<<endl;
 	    t--;
 	}
 	return 0;
 }
+
